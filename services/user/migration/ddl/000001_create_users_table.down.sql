ca54b08b7d542930314ef7f29984ed30d69e2b63
takuya911 <takuya.engineer911@gmail.com>
2021-01-01 20:14:30 +0900

    sql変更

---
+++ services/user/migration/ddl/000001_create_users_table.down.sql
@@ -0,0 +1,13 @@
+create type gender as enum ('女性', '男性', 'その他');
+
+CREATE TABLE IF NOT EXISTS users (
+  user_uuid uuid not null default gen_random_uuid(),
+  name varchar not null,
+  email varchar not null,
+  password varchar not null,
+  telephone_number varchar not null,
+  type gender not null,
+  created_at timestamp not null DEFAULT current_timestamp,
+  updated_at timestamp not null DEFAULT current_timestamp,
+  constraint users_pkey primary key (user_uuid),
+);
