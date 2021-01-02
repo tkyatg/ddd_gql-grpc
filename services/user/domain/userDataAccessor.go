5778167cf88987a958c91f58563c23a3f44a735f
takuya911 <takuya.engineer911@gmail.com>
2021-01-02 17:25:53 +0900

    空sql

--- services/user/domain/userDataAccessor.go
+++ services/user/domain/userDataAccessor.go
@@ -1,6 +1,8 @@
 package domain
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+)
 
 type (
 	userDataAccessor struct {
@@ -16,11 +18,48 @@ func NewUserDataAccessor(
 }
 
 func (d userDataAccessor) create(attr *UserAttributes) error {
+	sql := `
+insert into users.users
+     ( name
+     , email
+     , password
+     , telephone_number
+     , gender )
+values ( ?
+       , ?
+       , ?
+       , ?
+       , ? );
+`
+	if result := d.db.Exec(sql, attr.name, attr.email, attr.password, attr.telephoneNumber, attr.gender); result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
+
 func (d userDataAccessor) update(id UserUUID, attr *UserAttributes) error {
+	sql := `
+update users.users 
+   set name = ?
+     , email = ?
+     , password = ?
+     , telephone_number = ?
+     , gender = ?
+ where user_uuid = ?;
+`
+	if result := d.db.Exec(sql, attr.name, attr.email, attr.password, attr.telephoneNumber, attr.gender, id); result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
+
 func (d userDataAccessor) delete(id UserUUID) error {
+	sql := `
+delete users.users 
+ where user_uuid= ?;
+`
+	if result := d.db.Exec(sql, id); result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
